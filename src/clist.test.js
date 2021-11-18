061c917d184474bce7d389b8ceae2d1a548e29bd
Johan Gustafsson <johan.gustafsson@evolvetechnology.se>
2021-11-18 17:55:28 +0100

    Add tests in one file

--- src/clist.test.js
+++
@@ -1,6 +0,0 @@
-const { clist } = require("./clist");
-
-test(clist.name, () => {
-  expect(clist(1, 2, 3)).toEqual([1, 2, 3]);
-  expect(clist({ one: 1 }, { two: 2 })).toEqual([{ one: 1 }, { two: 2 }]);
-});
--- src/exercices.test.js
+++ src/exercices.test.js
@@ -3,6 +3,9 @@ const {
   calculateDepthOfList,
   findLargestValueInList,
 } = require("./recursion");
+const { clist } = require("./clist");
+const { add, sub, negate } = require("./add-sub");
+const { compose } = require("./compose");
 
 test(calculateSumOfNumbersInList.name, () => {
   const input = [1, [2], [[3]], 4, [[[[[[5]]]]]]];
@@ -20,4 +23,25 @@ test(findLargestValueInList.name, () => {
   const input = [1, [2], [[3]], [[[[7]]]], 6];
   expect(findLargestValueInList(input)).toBe(7);
   expect(findLargestValueInList(input.reverse())).toBe(7);
+});
+
+test(clist.name, () => {
+  expect(clist(1, 2, 3)).toEqual([1, 2, 3]);
+  expect(clist({ one: 1 }, { two: 2 })).toEqual([{ one: 1 }, { two: 2 }]);
+});
+
+test(add.name, () => {
+  expect(add(1, 2, 3)).toEqual(6);
+});
+
+test(sub.name, () => {
+  expect(sub(5, 1, 2)).toEqual(2);
+  expect(sub(5)).toEqual(-5);
+  expect(sub(-5)).toEqual(5);
+});
+
+test(compose.name, () => {
+  const double = (val) => val * 2;
+  expect(compose(negate, double, add)(1, 2, 3)).toEqual(-12);
+  expect(compose(clist, double, sub)(1, 2, 3)).toEqual([-8]);
 });
